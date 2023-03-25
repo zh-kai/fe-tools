e3e5069336f5ba4e13591cddbe63db9c7bcd1da0
zh-kai <zh_kai0531@163.com>
2023-03-25 18:34:15 +0800

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   build: {},
-  app: {
-    baseURL: '/fe-tools/',
-  },
   postcss: {
     plugins: {
       tailwindcss: {},
