077edee4c5e902f6d4189ce72c9fb3938b767d66
zh-kai <zh_kai0531@163.com>
2023-06-19 16:40:11 +0800

    fix: nuxt version & new links

--- pages/index.vue
+++ pages/index.vue
@@ -11,13 +11,18 @@
           <i class="fa-solid fa-palette"></i>
           <span>Color Conversions</span>
         </CustomLink>
+
+        <CustomLink href="https://easings.net/zh-cn">
+          <i class="fa-solid fa-play"></i>
+          <span>Easing Functions</span>
+        </CustomLink>
       </div>
     </div>
 
     <div>
       <h2 class="flex items-center gap-2 mb-4 text-2xl font-semibold">
         <IconsZksync class="w-6 mt-1" />
-        <span>zkSync</span>
+        <span>zkSync Resource</span>
       </h2>
 
       <div class="flex items-center gap-6 p-2">
@@ -60,13 +65,17 @@
     <div>
       <h2 class="flex items-center gap-2 mb-4 text-2xl font-semibold">
         <i class="fa-solid fa-robot text-lg"></i>
-        <span>AI</span>
+        <span>AI Resource</span>
       </h2>
 
       <div class="flex gap-6 p-2">
         <CustomLink href="https://poe.com/">
           <IconsAiPoe class="w-4" />
           <span>Poe</span>
+        </CustomLink>
+
+        <CustomLink href="https://prompthero.com/">
+          <span>Prompt Hero</span>
         </CustomLink>
       </div>
     </div>
